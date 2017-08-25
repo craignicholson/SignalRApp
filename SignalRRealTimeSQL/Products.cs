7a400193462a8568bb82ed29c88dda9bee83565e
Craig Nicholson <cnicholson@electsolve.com>
2017-08-24 19:58:24 -0500

    Initial Commit

---
+++ SignalRRealTimeSQL/Products.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SignalRRealTimeSQL
+{
+    public class Products
+    {
+        public string WebService { get; set; }
+        public string Method { get; set; }
+        public DateTime TimeReceived { get; set; }
+        public string TransactionID { get; set; }
+        public string Note { get; set; }
+        public string RequestXML { get; set; }
+
+    }
+}
---
+++ SignalRRealTimeSQL/index.aspx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SignalRRealTimeSQL
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        [WebMethod]
+        public static IEnumerable<Products> GetData()
+        {
+
+            using (var connection = new SqlConnection(ConfigurationManager.ConnectionStrings["DataBase"].ConnectionString))
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand(@"SELECT WebService,Method,TimeReceived,TransactionID, Note, RequestXML FROM IncomingRequest ORDER BY TimeReceived DESC;", connection))
+                {
+                    // Make sure the command object does not already have
+                    // a notification object associated with it.
+                    command.Notification = null;
+                    SqlDependency.Start(ConfigurationManager.ConnectionStrings["DataBase"].ConnectionString);
+                    SqlDependency dependency = new SqlDependency(command);
+                    dependency.OnChange += new OnChangeEventHandler(dependency_OnChange);
+
+                    if (connection.State == ConnectionState.Closed)
+                        connection.Open();
+
+                    using (var reader = command.ExecuteReader())
+                        return reader.Cast<IDataRecord>()
+                            .Select(x => new Products()
+                            {
+                                WebService = x.GetString(0),
+                                Method = x.GetString(1),
+                                TimeReceived = x.GetDateTime(2),
+                                TransactionID = x.GetString(3),
+                                Note = x.GetString(4),
+                                //RequestXML = x.GetString(5)
+                            }).ToList();
+                }
+            }
+        }
+        private static void dependency_OnChange(object sender, SqlNotificationEventArgs e)
+        {
+            MyHub.Show();
+        }
+
+    }
+}
